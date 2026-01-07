24663ef9e9cdc09a41b4125aceb5858d1d494dc2
=lavkeshraghav <lavkeshraghav190@gmail.com>
2026-01-07 21:53:35 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const repoName = "PORTFOLIO";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  basePath: `/${repoName}`,
   images: {
     domains: ["localhost", "api.charlinasweeps.co.in"],
     unoptimized: true,
